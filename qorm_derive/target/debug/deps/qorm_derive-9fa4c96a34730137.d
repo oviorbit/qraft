ba9384b9c4d5069ddb8330f9cead23a5714fd178
Dany Gagnon <dany@ovior.ca>
2025-06-21 23:16:48 -0400

    fix: rows affected

--- qorm_derive/target/debug/deps/qorm_derive-9fa4c96a34730137.d
+++
@@ -1,9 +0,0 @@
-/home/ovior/qrafting/qraft/qorm_derive/target/debug/deps/libqorm_derive-9fa4c96a34730137.rmeta: src/lib.rs Cargo.toml
-
-/home/ovior/qrafting/qraft/qorm_derive/target/debug/deps/qorm_derive-9fa4c96a34730137.d: src/lib.rs Cargo.toml
-
-src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
--- qorm_derive/target/debug/deps/qorm_derive-ecb8012e51b6d484.d
+++
@@ -1,9 +0,0 @@
-/home/ovior/qrafting/qraft/qorm_derive/target/debug/deps/libqorm_derive-ecb8012e51b6d484.rmeta: src/lib.rs Cargo.toml
-
-/home/ovior/qrafting/qraft/qorm_derive/target/debug/deps/qorm_derive-ecb8012e51b6d484.d: src/lib.rs Cargo.toml
-
-src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
